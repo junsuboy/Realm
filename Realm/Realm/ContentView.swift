c59954791a46e28d94977aee4b606a7ff619cd78
Junsuboy <86935775+junsuboy@users.noreply.github.com>
2022-06-17 15:20:06 +0900

    created Realm App

---
+++ Realm/Realm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Realm
+//
+//  Created by Junsu Jang on 2022/06/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Realm/Realm/RealmApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmApp.swift
+//  Realm
+//
+//  Created by Junsu Jang on 2022/06/17.
+//
+
+import SwiftUI
+
+@main
+struct RealmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
